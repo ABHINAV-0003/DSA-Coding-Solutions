838894e1304a81629d54f6e9f84780528c477e6f
APEACE <140238037+ABHINAV-0003@users.noreply.github.com>
2024-07-28 12:03:45 +0530

    Create Null_pointer.cpp

---
+++ Null_pointer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int* ptr = nullptr;
+
+    if (ptr == nullptr) {
+        cout << "Null pointer indicates the end of this journey.\n Thank You." << endl;
+    }
+
+    return 0;
+}
